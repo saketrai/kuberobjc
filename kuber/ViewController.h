21887d13470f8ea40945cbfc7a8d4adfc85f4ce6
Saket <saketrai@gmail.com>
2015-06-26 21:58:43 -0700

    Initial Commit

---
+++ kuber/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kuber
+//
+//  Created by Saket Rai on 6/26/15.
+//  Copyright (c) 2015 Saket Rai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ kuber/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kuber
+//
+//  Created by Saket Rai on 6/26/15.
+//  Copyright (c) 2015 Saket Rai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
